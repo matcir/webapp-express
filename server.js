5d27416db8bfbb0293a3d90be9386673ac4b7c55
matcir <matteocirilloboolean@gmail.com>
2025-08-01 14:02:39 +0200

    implemented middleware cors

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const movieRouter = require("./routes/movies");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.static("public"));
 app.use("/api/movies", movieRouter);
 
