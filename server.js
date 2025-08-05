6b087709f3ede698b9c58c75e5fbb58b993296bc
matcir <matteocirilloboolean@gmail.com>
2025-08-05 10:31:00 +0200

    implemented 404 handler on fetching data

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT;
 const movieRouter = require("./routes/movies");
 const cors = require("cors");
+const routeNotFound = require("./middlewares/routeNotFound");
+const serverErrorHandler = require("./middlewares/serverErrorHandler");
 
 app.use(cors());
 app.use(express.static("public"));
@@ -11,6 +13,9 @@ app.use("/api/movies", movieRouter);
 app.get("/", (req, res) => {
   res.send("Welcome to my app");
 });
+
+app.use(routeNotFound);
+app.use(serverErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
